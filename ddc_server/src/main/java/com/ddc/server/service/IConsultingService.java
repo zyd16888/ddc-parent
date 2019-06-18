bb0fde6841677510a472756532ef719a60da06c9
zyd16888 <zyd475182659@163.com>
2019-06-18 10:31:51 +0800

    咨询、图片、图片列表Service接口和Impl实现类

---
+++ ddc_server/src/main/java/com/ddc/server/service/IConsultingService.java
@@ -0,0 +1,13 @@
+package com.ddc.server.service;
+
+import com.baomidou.mybatisplus.service.IService;
+import com.ddc.server.entity.Consulting;
+
+/**
+ * @author dong
+ * @since 2019-06-18
+ */
+
+public interface IConsultingService extends IService<Consulting> {
+
+}
